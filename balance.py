fb7c0d3609b09bbcb92628e4254ae1c84837fa0f
Pseudobeard <Sonny.Dineen@gmail.com>
2017-05-28 15:25:33 -0500

    Updated balance to use simple weighting

--- balance.py
+++ balance.py
@@ -9,6 +9,24 @@ def readPlayers(fileName):
 def addPlayer(playerName, SR ):
     players[playerName] = SR
     return
+
+def getWeight(SR):
+    weight = 0.2
+    if SR > 1000:
+        weight = 0.4
+    if SR > 1500:
+        weight = 0.6
+    if SR > 2000:
+        weight = 0.8
+    if SR > 2500:
+        weight = 1
+    if SR > 3000:
+        weight = 1.2
+    if SR > 3500:
+        weight = 1.4
+    if SR > 4000:
+        weight = 1.6
+    return weight
   
 #Grab the list of players
 players = {}
@@ -16,24 +34,30 @@ readPlayers('players.txt')
 
 #Create the two teams
 redTeam = {}
-redTeamSR = 0
+redTeamAverageSR = 0
+redTeamWeightedSR = 0
 blueTeam = {}
-blueTeamSR = 0
+blueTeamAverageSR = 0
+blueTeamWeightedSR = 0
 
-#Greedy algorithm. Sort by SR and pop off
+#Greedy algorithm. Sort by weighted SR and pop off
 for x in sorted(players, key=players.get):
-    if redTeamSR < blueTeamSR:
-        redTeam[x] = players[x]
-        redTeamSR += int(redTeam[x])
+    SR = int(players[x])
+    WSR = float(players[x]) * getWeight(players[x])
+    if redTeamWeightedSR < blueTeamWeightedSR:
+        redTeam[x] = SR
+        redTeamWeightedSR += WSR
+        redTeamAverageSR += SR
     else:
-        blueTeam[x] = players[x]
-        blueTeamSR += int(blueTeam[x])
+        blueTeam[x] = SR
+        blueTeamWeightedSR += WSR
+        blueTeamAverageSR += SR
 
 #Print the teams
-print ("Red Team Average SR: " + str((redTeamSR)/len(redTeam)))
+print ("Red Team Average SR: " + str((redTeamAverageSR)/len(redTeam)))
 for i in redTeam:
     print (i, redTeam[i])
 print ("------------")
-print ("Blue Team Average SR: " + str((blueTeamSR)/len(blueTeam)))
+print ("Blue Team Average SR: " + str((blueTeamAverageSR)/len(blueTeam)))
 for i in blueTeam:
     print (i, blueTeam[i])
