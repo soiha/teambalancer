00bbe68c6d45e8d058e4385ba294c9d51e70480c
Joe Cellucci <celluj34@gmail.com>
2017-05-29 17:48:25 -0500

    format using pycharm's default rules

--- balance.py
+++ balance.py
@@ -5,13 +5,13 @@ import player
 def readPlayers(fileName):
     filePlayers = []
     f = open(fileName, 'r')
-    scraper = scraper.Scraper()
+    s = scraper.Scraper()
     for line in f:
         playerID = line[:-1]
-        print playerID
-        player = player.Player(playerID)
-        filePlayers.append(player)
-        scraper.scrape(player)
+        print(playerID)
+        p = player.Player(playerID)
+        s.scrape(p)
+        filePlayers.append(p)
     f.close()
     return filePlayers
 
@@ -26,24 +26,25 @@ def partition(playerList):
     blueTeamAverageSR = 0
     blueTeamWeightedSR = 0
     for p in playerList:
-        print ("  Sorting " + p.getName())
+        print("  Sorting " + p.getName())
         if redTeamWeightedSR < blueTeamWeightedSR:
             redTeam.append(p)
             redTeamWeightedSR += p.getWeightedSR()
             redTeamAverageSR += p.getSR()
-            print ("    Sorted " + p.getName() + " to red team")
+            print("    Sorted " + p.getName() + " to red team")
         else:
             blueTeam.append(p)
             blueTeamWeightedSR += p.getWeightedSR()
             blueTeamAverageSR += p.getSR()
-            print ("    Sorted " + p.getName() + " to blue team")
+            print("    Sorted " + p.getName() + " to blue team")
     return redTeam, blueTeam
-    
+
+
 # Gonna make it look real nice
 def printTeam(team):
     for p in team:
         string = '{:14}'.format(p.getName()) + '{:4}'.format(p.getSR()) + '{:>18}'.format(p.getRole())
-        print ('| ' + string + ' |')
+        print ('| %s |' % string)
      
 
 if __name__ == "__main__":
@@ -52,7 +53,7 @@ if __name__ == "__main__":
     players.sort(key=lambda x: x.getSR(), reverse=True)
 
     # Greedy algorithm. Sort by weighted SR and pop off
-    print ("Begin Sorting")
+    print("Begin Sorting")
     redTeam, blueTeam = partition(players)
     print("Sorting complete")
     print("----------------------------------------")
